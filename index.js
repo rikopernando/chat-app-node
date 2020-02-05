c89132402f717d42b221c23046f01f352983616e
rikopernando <riko.pernando@bareksa.com>
2020-02-05 14:32:40 +0700

    socket io ON

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const socket = require('socket.io')
 const cors = require('cors')
-const models = require('./models')
+const {Message} = require('./models')
 const app = express()
 const port = process.env.PORT || 7000
 
@@ -19,18 +19,28 @@ let io =  socket(server);
 
 io.on("connection", function(socket){
   console.log("Socket Connection Established with ID :"+ socket.id)
+
   socket.on("chat", async function(chat){
-    chat.created = new Date()
-    console.log(chat)
+    console.log(chat, "disini")
     // let response = await new message(chat).save()
-    socket.emit("chat",chat)
+    Message.create({
+      handle: "sample",
+      message: chat
+    })
+    .then(resp => {
+      //console.log(resp)
+      io.sockets.emit("chat", resp)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   })
 })
 
 
 app.get('/chat', async (req,res) => {
   //let result = await message.find()
-  models.Message.findAll()
+  Message.findAll()
     .then(resp => {
       res.status(200).json({
         data: resp,
