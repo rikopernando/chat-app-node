dde70304cbde14f60658aaf8eed5d2480a289249
Riko <rikopernando96@gmail.com>
2020-02-10 09:42:23 +0700

    exit menu

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/chat', async (req,res) => {
       attributes: ['id', 'message', 'user_id', 'createdAt'],
       include: [{
         model: Users,
-        attributes: ['name']
+        attributes: ['id','name']
       }]
     })
     .then(resp => {
